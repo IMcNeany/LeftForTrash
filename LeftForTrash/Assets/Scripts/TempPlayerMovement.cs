6ab26006ee2728ddfc56247625d2db8d476fe7ba
D'Arcy Sullivan <d_sullivan@hotmail.co.uk>
2018-10-28 12:38:32 +0000

    Basic level design (will be reworked)
    proof of concept for floor levels
    basic player movement for testing
    Ill tidy the design later.

---
+++ LeftForTrash/Assets/Scripts/TempPlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private Vector2 movement;
+    public float speed = 5.0f;
+
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+        rb2d.MovePosition(rb2d.position + movement * Time.fixedDeltaTime);
+    }
+}
---
+++ LeftForTrash/Assets/Scripts/TriggerFloor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFloor : MonoBehaviour
+{
+    public GameObject floor00;
+    public GameObject floor01;
+
+    public void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (floor00.activeInHierarchy)
+            {
+                floor00.SetActive(false);
+                floor01.SetActive(true);
+            }
+            else if (floor01.activeInHierarchy)
+            {
+                floor01.SetActive(false);
+                floor00.SetActive(true);
+            }
+        }
+    }
+}
