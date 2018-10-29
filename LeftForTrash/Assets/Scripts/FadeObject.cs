a3834cf6f94c371ebfcd2ca2181bedf7e8655b7e
D'Arcy Sullivan <d_sullivan@hotmail.co.uk>
2018-10-29 16:39:44 +0000

    Room prefabs, will fix stage 01 with new design

---
+++ LeftForTrash/Assets/Scripts/FadeObject.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeObject : MonoBehaviour
+{
+    public bool start = false;
+    private Color target_colour;
+    public float alpha = 0;
+    public bool fadeIn = false;
+
+    private void Start()
+    {
+        target_colour = gameObject.GetComponent<SpriteRenderer>().material.color;
+    }
+
+    private void Update()
+    {
+        if(start)
+        {
+            if(fadeIn)
+            {
+                FadeIn();
+            }
+            else
+            {
+                FadeOut();
+            }
+
+            if (target_colour.a <= 0.0f)
+            {
+                target_colour.a = 0.0f;
+                start = false;
+            }
+            if(target_colour.a >= 1.0f)
+            {
+                target_colour.a = 1.0f;
+                start = false;
+            }
+        }
+
+        alpha = target_colour.a;
+
+        gameObject.GetComponent<SpriteRenderer>().material.color = target_colour;
+    }
+
+    private void FadeOut()
+    {
+        target_colour.a -= 1.0f * Time.deltaTime;
+    }
+
+    private void FadeIn()
+    {
+        target_colour.a += 1.0f * Time.deltaTime;
+    }
+}
