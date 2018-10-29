a07f8340cc8d7a8ef0c9965c8d14625e20ad8d85
Sam Hibb <samhibbert13@yahoo.co.uk>
2018-10-29 16:00:40 +0000

    adding basic input manager system + some additional test scripts and prefabs

---
+++ LeftForTrash/Assets/Scripts/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	public int playerNumber;
+
+	private float Horizontal, Vertical;
+	private bool[] buttons = new bool[4];
+
+	// Update is called once per frame
+	void Update () {
+		Horizontal = Input.GetAxis("Horizontal_" + playerNumber);
+		Vertical = Input.GetAxis("Vertical_" + playerNumber);
+
+		buttons[0] = Input.GetButton("A_" + playerNumber);
+		buttons[1] = Input.GetButton("B_" + playerNumber);
+		buttons[2] = Input.GetButton("X_" + playerNumber);
+		buttons[3] = Input.GetButton("Y_" + playerNumber);
+	}
+
+	public float getHorizontal(){ return Horizontal; }
+	public float getVertical(){ return Vertical; }
+
+	/*0 - A, 1 - B, 2 - X, 3 - Y */
+	public bool[] getButtons(){
+		return buttons;
+	}
+}
+
+
---
+++ LeftForTrash/Assets/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private Quaternion rotationDirection;
+    private InputManager input;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb2d;
+    private Vector2 movement;
+
+    
+
+    public float speed = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+        input = GetComponent<InputManager>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateMovement();
+
+        if(input.getButtons()[0]){
+            spriteRenderer.color = Color.green;
+        } else if(input.getButtons()[1]){
+            spriteRenderer.color = Color.red;
+        } else if(input.getButtons()[2]){
+            spriteRenderer.color = Color.blue;
+        } else if(input.getButtons()[3]){
+            spriteRenderer.color = Color.yellow;
+        }
+	}
+
+    void UpdateMovement()
+    {
+        movement = new Vector2(input.getHorizontal() * speed, input.getVertical() * speed);
+      
+
+        if (movement != Vector2.zero) {
+            Vector3 eulerRotation = Quaternion.LookRotation(Vector3.forward, movement).eulerAngles;
+            eulerRotation.z = Mathf.Round(eulerRotation.z / 90) * 90;
+            transform.rotation = Quaternion.Euler(eulerRotation);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+          rb2d.MovePosition(rb2d.position + movement * Time.fixedDeltaTime);
+        
+    }
+}
+
