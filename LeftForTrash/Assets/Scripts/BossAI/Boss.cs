41d97885fd9a33431c0537fc1a3ed561568b3532
SamHibb <>
2018-10-31 15:25:14 +0000

    added animation and auto attack for boss

---
+++ LeftForTrash/Assets/Scripts/BossAI/Boss.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+
+	public enum State{
+		WAIT,
+		ATTACK_1
+		
+	}
+
+	//public UI healthbar
+
+	public float health;
+	public float damageScale;
+	public float waitTime;
+	public State state; //Public for debug
+
+	protected float time;
+	protected Animator animator;
+
+	// Use this for initialization
+	protected void setup () {
+		state = State.WAIT;
+		animator = GetComponent<Animator>();
+		time = Time.time;
+	}
+
+	protected void update(){
+
+	}
+}
---
+++ LeftForTrash/Assets/Scripts/BossAI/JCB.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JCB : Boss {
+
+	// Use this for initialization
+	void Start () {
+		base.setup();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		base.update();
+
+		if((Time.time - time) >= waitTime && state == State.WAIT)
+		{
+			state = State.ATTACK_1;
+			time = Time.time;
+		}else if(animator.GetCurrentAnimatorStateInfo(0).IsName("JCB_idle")){
+			state = State.WAIT;
+			animator.SetBool("attack_1", false);
+		}
+
+		processState();
+	}
+
+	private void processState(){
+		switch(state){
+			case State.ATTACK_1:
+			//Attack logic
+			animator.SetBool("attack_1", true);
+			break;
+			case State.WAIT:
+			//Do nothing
+			break;
+		}
+	}	
+}
