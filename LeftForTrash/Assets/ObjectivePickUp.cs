56a8f28a936b6c655b1a575ab39af789fad9ffe2
D'Arcy Sullivan <d_sullivan@hotmail.co.uk>
2018-11-05 21:14:18 +0000

    Stage 2 Objective markers and exit fade

---
+++ LeftForTrash/Assets/ObjectivePickUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectivePickUp : MonoBehaviour
+{
+    public GameObject exit_script;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        exit_script.GetComponent<Stage2EndCheck>().objective_count--;
+        Destroy(this.gameObject);
+    }
+}
