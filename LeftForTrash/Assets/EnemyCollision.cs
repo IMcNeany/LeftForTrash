dac034573ad91447a5c2bd23225d11c7c7e9c808
Unknown <Elliot2.martin@live.uwe.ac.uk>
2018-11-05 18:03:48 +0000

    Got player2 spin attack to damage enemies

--- LeftForTrash/Assets/EnemyCollision.cs
+++ LeftForTrash/Assets/EnemyCollision.cs
@@ -6,6 +6,8 @@ public class EnemyCollision : MonoBehaviour {
 
     public EnemyBehaviour EB;
     public Boss boss;
+    private float damage_tick = 0.2f;
+    private bool spin_damage = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,7 +16,25 @@ public class EnemyCollision : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//this is for p2 spin attack sorry about spaghetti code
+        if(spin_damage)
+        {
+            damage_tick -= 1 * Time.deltaTime;
+            if(damage_tick <= 0.0f)
+            {
+                float damage = 20;
+                if (EB)
+                {
+                    EB.TakeDamage(damage);
+                }
+
+                if (boss)
+                {
+                    boss.hit(damage);
+                }
+                damage_tick = 0.2f;
+            }
+        }
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -41,5 +61,20 @@ public class EnemyCollision : MonoBehaviour {
         }
 
 
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.name == "Spin_Hitbox")
+        {
+            spin_damage = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "Spin_Hitbox")
+        {
+            spin_damage = false;
+        }
     }
 }
