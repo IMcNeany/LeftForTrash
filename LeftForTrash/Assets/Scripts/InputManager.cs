8f8ba9a6d0eff217738a10cc9faec90565a87d4c
SamHibb <samhibbert13@yahoo.co.uk>
2018-11-05 17:57:44 +0000

    adding right analog stick and trigger

--- LeftForTrash/Assets/Scripts/InputManager.cs
+++ LeftForTrash/Assets/Scripts/InputManager.cs
@@ -6,13 +6,18 @@ public class InputManager : MonoBehaviour {
 
 	public int playerNumber;
 
-	private float Horizontal, Vertical;
+	private float Horizontal, Vertical, AimHorizontal, AimVertical, Trigger;
 	private bool[] buttons = new bool[4];
 
 	// Update is called once per frame
 	void Update () {
 		Horizontal = Input.GetAxis("Horizontal_" + playerNumber);
 		Vertical = Input.GetAxis("Vertical_" + playerNumber);
+		
+		AimHorizontal = Input.GetAxis("Aim_Horizontal_" + playerNumber);
+		AimVertical = Input.GetAxis("Aim_Vertical_" + playerNumber);
+		
+		Trigger = Input.GetAxis("Trigger_" + playerNumber);
 
 		buttons[0] = Input.GetButton("A_" + playerNumber);
 		buttons[1] = Input.GetButton("B_" + playerNumber);
@@ -22,6 +27,11 @@ public class InputManager : MonoBehaviour {
 
 	public float getHorizontal(){ return Horizontal; }
 	public float getVertical(){ return Vertical; }
+
+	public float getAimHorizontal(){ return AimHorizontal; }
+	public float getAimVertical(){ return AimVertical; }
+
+	public float getTrigger(){ return Trigger; }
 
     public void setHorizontal(float x) { Horizontal = x; }
     public void setVertical(float y) { Vertical = y; }
